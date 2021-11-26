1fd8f686e3f96240b97cc61ec4e1344b27c2702f
Your Name <you@example.com>
2021-11-26 10:32:39 +0700

    baithi

---
+++ LeThanhDat-T2009M1/Entities/Contact.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeThanhDat_T2009M1.Entities
+{
+    public class Contact
+    {
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
